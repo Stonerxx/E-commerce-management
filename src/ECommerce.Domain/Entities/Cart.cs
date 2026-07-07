41d505313db298d946f7126be599b3b2c6976122
Mavscholangki <zsh_0215@qq.com>
2026-07-07 10:27:17 +0800

    feat(member4)：添加所需实体类

---
+++ src/ECommerce.Domain/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerce.Domain.Entities;
+
+public class Cart
+{
+    public long Id { get; set; }
+
+    public long UserId { get; set; }
+
+    public long SkuId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int Selected { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ src/ECommerce.Domain/Entities/OrderLog.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerce.Domain.Entities;
+
+public class OrderLog
+{
+    public long Id { get; set; }
+
+    public long OrderId { get; set; }
+
+    public int? FromStatus { get; set; }
+
+    public int ToStatus { get; set; }
+
+    public long? OperatorId { get; set; }
+
+    public string? Remark { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
