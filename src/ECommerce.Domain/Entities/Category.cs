155366561934788f4340992eaf85bf9aa00ca7fb
Mavscholangki <zsh_0215@qq.com>
2026-07-07 08:59:31 +0800

    feat(member3)
    添加模块相关的实体类

---
+++ src/ECommerce.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public int? ParentId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int TreeLevel { get; set; }
+    public int SortOrder { get; set; }
+    public int Status { get; set; }
+    public string? IconUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
